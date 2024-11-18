880d6ecbb22e56e82e3ab131dc59414ab2229b58
unknown <kuldeepgangarekar2001@gmail.com>
2024-11-18 18:06:23 +0530

    This is for Pime Number

---
+++ add.js
@@ -0,0 +1,23 @@
+function primeNo(arr){
+    let primNumber = [];
+    
+    if(arr[i]<2) isprime=false;
+    for(let i=3;i<arr.length;i++){
+        let isprime = true;
+        for(let j=2;j<i;j++){
+            if(i%j===0) isprime=false;
+        }
+        if(isprime===true) primeNumber.push(arr[i]);
+    }
+   let min = primNumber[0];
+    let max = primNumber[0];
+    let sum = 0;
+    for(let i=0;i<primeNumber.length;i++){
+        if(primNumber[i]<min) min = primNumber[i];
+        if(primNumber[i]>max) max = primNumber[i];
+        sum += primNumber[i];
+    }
+    console.log(max);
+    console.log(min);
+    console.log(sum);
+}
